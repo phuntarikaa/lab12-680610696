63a12df16a2b30dbacadb5ee4ca352c09dd6b593
phuntarikaa <phuntarikaa@gmail.com>
2026-09-04 16:58:04 +0700

    edit

--- src/components/TaskInput.tsx
+++ src/components/TaskInput.tsx
@@ -1,8 +1,10 @@
 export default function TaskInput() {
   return (
-    <div className="d-flex my-3">
-      <input className="form-control me-2" type="text" placeholder="TODO" />
-      <button className="btn btn-primary px-4">Add</button>
-    </div>
+    <input
+      className="form-control me-2"
+      type="text"
+      placeholder="Insert a task here.."
+    />
   );
 }
+
---
+++ src/libs/Footer.ts
@@ -0,0 +1,7 @@
+interface FooterProps {
+  year: string;
+  fullName: string;
+  studentId: string | number; // union type: string หรือ number ก็ได้
+}
+
+export type { FooterProps };
